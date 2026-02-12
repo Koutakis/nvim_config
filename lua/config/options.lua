48feb74433d9ca92096ee0d562224d29ff095559
Koutakis <alexander.koutakis00@gamil.com>
2026-02-12 10:10:44 +0100

    added functionality for tunneling nvim clipboard to windows clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,3 +41,16 @@ vim.api.nvim_create_autocmd("InsertLeave", {
         end
     end,
 })
+
+-- Enable OSC 52 clipboard over SSH
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
