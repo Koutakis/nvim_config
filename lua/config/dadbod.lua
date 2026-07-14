97815584ca48f872174402a69b319c79d60e15fb
Koutakis <189662150+Koutakis@users.noreply.github.com>
2026-07-14 10:35:07 +0200

    added dadbod connection

--- lua/config/dadbod.lua
+++ lua/config/dadbod.lua
@@ -2,5 +2,6 @@ vim.g.dbs = {
     { name = "Ekonomi", url = os.getenv("BIG_EKONOMI_EXECUTION_PROD") },
     { name = "VARD", url = os.getenv("BIG_VARD_EXECUTION_PROD") },
     { name = "FRAPP", url = os.getenv("BIG_FRAPP_EXECUTION_PROD")} ,
-    { name = "STATISTIK", url = os.getenv("BIG_STATISTIK_EXECUTION_PROD") }
+    { name = "STATISTIK", url = os.getenv("BIG_STATISTIK_EXECUTION_PROD") },
+    { name = "BIRTH_CID", url = os.getenv("BIRTH_CID_DADBOD") }
 }
